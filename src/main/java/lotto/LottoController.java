4fb5d8dd954fb4bea37b466dd8a6651b922cc7b1
koomin1227 <koomin1227@naver.com>
2024-01-09 17:00:27 +0900

    feat: 로또의 개수를 저장하는 기능 구현

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public class LottoController {
+    private int lottoCount = 0;
+    public void run() {
+        createLottoCount();
+    }
+
+    private void createLottoCount() {
+        while (true) {
+            try {
+                int price = Input.inputPrice();
+                lottoCount = price / 1000;
+                return;
+            } catch (IllegalArgumentException e) {
+                Input.printError(e.getMessage());
+            }
+        }
+    }
+}
