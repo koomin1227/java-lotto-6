ca5814036976114ad203037fb38c75a0b64bf652
koomin1227 <koomin1227@naver.com>
2024-01-10 20:39:12 +0900

    feat: 문자열에서 숫자를 파싱하는 클래스 구현

--- src/main/java/lotto/Domain/Number.java
+++ src/main/java/lotto/Domain/Number.java
@@ -2,26 +2,19 @@ package lotto.Domain;
 
 public class Number {
     private final int number;
-    public Number(String input) {
-        validate(input);
-        this.number = Integer.parseInt(input);
+    public Number(int number) {
+        validate(number);
+        this.number = number;
     }
 
-    private void validate(String input) {
-        if (!isNumeric(input)) {
-            throw new IllegalArgumentException("숫자로만 이루어져있지 않음");
-        }
+    public static void validate(int input) {
         if (!isValidLottoNumber(input)) {
             throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
-    private static boolean isNumeric(String input) {
-        return input.matches("\\d+");
-    }
-    private static boolean isValidLottoNumber(String input) {
-        int num = Integer.parseInt(input);
-        return num > 0 && num <= 45;
+    private static boolean isValidLottoNumber(int number) {
+        return number > 0 && number <= 45;
     }
 
     public int getNumber() {
