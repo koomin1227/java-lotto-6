1f33d9c721f8727e84a1d491cef24a2f4719c1b6
koomin1227 <koomin1227@naver.com>
2024-01-10 23:22:03 +0900

    feat: 로또 당첨 결과를 다루는 WinningResult 구현

---
+++ src/main/java/lotto/Domain/WinningResult.java
@@ -0,0 +1,46 @@
+package lotto.Domain;
+
+
+import lotto.LottoPrize;
+import lotto.MatchingResult;
+
+public class WinningResult {
+    private final int[] result = new int[5];
+    private final int price;
+    private int totalProfit = 0;
+
+    public WinningResult(int price) {
+        this.price = price;
+    }
+
+    public void updateResult(MatchingResult matchingResult) {
+        if (LottoPrize.FIRST.getMatchNumber() == matchingResult.getMatchedCount()) {
+            result[LottoPrize.FIRST.getValue()] += 1;
+            totalProfit += LottoPrize.FIRST.getPrize();
+        }
+        if (LottoPrize.SECOND.getMatchNumber() == matchingResult.getMatchedCount() && matchingResult.isBonusMatch()) {
+            result[LottoPrize.SECOND.getValue()] += 1;
+            totalProfit += LottoPrize.SECOND.getPrize();
+        }
+        if (LottoPrize.THIRD.getMatchNumber() == matchingResult.getMatchedCount()) {
+            result[LottoPrize.THIRD.getValue()] += 1;
+            totalProfit += LottoPrize.THIRD.getPrize();
+        }
+        if (LottoPrize.FOURTH.getMatchNumber() == matchingResult.getMatchedCount()) {
+            result[LottoPrize.FOURTH.getValue()] += 1;
+            totalProfit += LottoPrize.FOURTH.getPrize();
+        }
+        if (LottoPrize.FIFTH.getMatchNumber() == matchingResult.getMatchedCount()) {
+            result[LottoPrize.FIFTH.getValue()] += 1;
+            totalProfit += LottoPrize.FIFTH.getPrize();
+        }
+    }
+
+    public int getPrizeCount(LottoPrize prize) {
+        return result[prize.getValue()];
+    }
+
+    public double getProfit() {
+        return ((double) price / totalProfit) * 100;
+    }
+}
