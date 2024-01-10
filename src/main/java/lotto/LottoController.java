14d08d63831dc8829780ed201386c60debedd8f6
koomin1227 <koomin1227@naver.com>
2024-01-10 19:03:57 +0900

    feat: 당첨 번호를 입력받고 저장하는 기능 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,9 +1,24 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
     private int lottoCount = 0;
+    private WinningNumbers winningNumbers;
+    private final ArrayList<Lotto> lottos = new ArrayList<>();
     public void run() {
         createLottoCount();
+        createLotto();
+        createWinningNumbers();
+    }
+
+    private void createLotto() {
+        for (int i = 0; i < lottoCount; i++) {
+            ArrayList<Integer> numbers = Numbers.createNumbers();
+            lottos.add(new Lotto(numbers));
+        }
+        Output.printLotto(lottos);
     }
 
     private void createLottoCount() {
@@ -16,5 +31,18 @@ public class LottoController {
                 Output.printError(e.getMessage());
             }
         }
+    }
+
+    private void createWinningNumbers() {
+        while (true) {
+            try {
+                ArrayList<String> winningNumbers = Input.inputWinningNumbers();
+                this.winningNumbers = new WinningNumbers(winningNumbers);
+                return;
+            } catch (IllegalArgumentException e) {
+                Output.printError(e.getMessage());
+            }
+        }
+
     }
 }
