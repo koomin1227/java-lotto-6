65812983adb760961c75e3bf4f637d1de8f8e519
koomin1227 <koomin1227@naver.com>
2024-01-11 00:28:27 +0900

    feat: 문자열 버전의 가격추가

--- src/main/java/lotto/Constant/LottoPrize.java
+++ src/main/java/lotto/Constant/LottoPrize.java
@@ -1,20 +1,22 @@
-package lotto;
+package lotto.Constant;
 
 public enum LottoPrize {
-    FIFTH(4, 5000, 3),
-    FOURTH(3, 50000, 4),
-    THIRD(2, 1500000, 5),
-    SECOND(1, 30000000, 5),
-    FIRST(0, 2000000000, 6);
+    FIFTH(4, 5000, 3, "5,000"),
+    FOURTH(3, 50000, 4, "50,000"),
+    THIRD(2, 1500000, 5, "1,500,000"),
+    SECOND(1, 30000000, 5, "30,000,000"),
+    FIRST(0, 2000000000, 6, "2,000,000,000");
 
     private final int value;
     private final int prize;
     private final int matchNumber;
+    private final String price;
 
-    LottoPrize(int value, int prize, int matchNumber) {
+    LottoPrize(int value, int prize, int matchNumber, String price) {
         this.value = value;
         this.prize = prize;
         this.matchNumber = matchNumber;
+        this.price = price;
     }
 
     public int getValue() {
@@ -27,5 +29,9 @@ public enum LottoPrize {
 
     public int getMatchNumber() {
         return matchNumber;
+    }
+
+    public String getPrice() {
+        return price;
     }
 }
