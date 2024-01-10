b8e5a27660e2d25232790c6a03892c43da8fb5ee
koomin1227 <koomin1227@naver.com>
2024-01-10 18:58:09 +0900

    test: WinningNumbers Test 코드 작성

---
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningNumbersTest {
+    @Test
+    void 숫자가_아닌_문자가_있는_경우_오류() {
+        String[] input = {"12", "8", "a123", "3", "5", "35"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(input));
+        assertThatThrownBy(() -> new WinningNumbers(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자의_범위가_넘는_경우_오류() {
+        String[] input = {"12", "8", "50", "3", "5", "35"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(input));
+        assertThatThrownBy(() -> new WinningNumbers(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 중복되는_경우_오류() {
+        String[] input = {"12", "8", "12", "3", "5", "35"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(input));
+        assertThatThrownBy(() -> new WinningNumbers(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_반환() {
+        String[] input = {"12", "8", "45", "3", "5", "35"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(input));
+        WinningNumbers winningNumbers = new WinningNumbers(inputs);
+        assertThat(winningNumbers.getWinningNumbers().size()).isEqualTo(6);
+    }
+}
