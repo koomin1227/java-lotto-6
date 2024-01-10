d20739d93f789d63511bc84d0a312ba0e0631a67
koomin1227 <koomin1227@naver.com>
2024-01-10 18:35:51 +0900

    feat: 로또 당첨 번호 클래스 구현

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningNumbers {
+    private List<Integer> winningNumbers;
+    public WinningNumbers(ArrayList<String> winningNumbers) {
+        validate(winningNumbers);
+        this.winningNumbers = winningNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+    private void validate(ArrayList<String> winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            if (isNumeric(winningNumber)) {
+                throw new IllegalArgumentException("숫자로만 이루어져있지 않음");
+            }
+            if (isValidLottoNumber(winningNumber)) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private static boolean isNumeric(String input) {
+        return input.matches("\\d+");
+    }
+    private static boolean isValidLottoNumber(String input) {
+        int num = Integer.parseInt(input);
+        return num > 0 && num <= 45;
+    }
+}
