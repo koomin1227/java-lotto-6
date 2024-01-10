3bd373edc522a8a40ab175db133eb95e3b3e76b6
koomin1227 <koomin1227@naver.com>
2024-01-10 23:34:05 +0900

    refactor: 사용하지 않는 필드 삭제

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -1,21 +1,20 @@
 package lotto;
 
 public enum LottoPrize {
-    FIRST(0, 2000000000, 6, false),
-    SECOND(1, 30000000, 5, true),
-    THIRD(2, 1500000, 5, false),
-    FOURTH(3, 50000, 4, false),
-    FIFTH(4, 5000, 3, false);
+    FIFTH(4, 5000, 3),
+    FOURTH(3, 50000, 4),
+    THIRD(2, 1500000, 5),
+    SECOND(1, 30000000, 5),
+    FIRST(0, 2000000000, 6);
 
-    private int value;
-    private int prize;
-    private int matchNumber;
-    private boolean isBonusMatch;
-    private LottoPrize(int value, int prize, int matchNumber, boolean isBonusMatch) {
+    private final int value;
+    private final int prize;
+    private final int matchNumber;
+
+    LottoPrize(int value, int prize, int matchNumber) {
         this.value = value;
         this.prize = prize;
         this.matchNumber = matchNumber;
-        this.isBonusMatch = isBonusMatch;
     }
 
     public int getValue() {
@@ -28,9 +27,5 @@ public enum LottoPrize {
 
     public int getMatchNumber() {
         return matchNumber;
-    }
-
-    public boolean getIsBonusMatch() {
-        return isBonusMatch;
     }
 }
