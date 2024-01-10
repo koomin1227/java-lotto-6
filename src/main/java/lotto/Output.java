61c9fb913e47c180f7c8007ccebdd004cb129a3f
koomin1227 <koomin1227@naver.com>
2024-01-10 17:21:12 +0900

    feat: 로또 번호 출력 함수 구현

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Output {
     public static void printError(String message) {
         System.out.printf("[ERROR] %s\n", message);
+    }
+
+    public static void printLotto(ArrayList<Lotto> lottos) {
+        System.out.printf("\n%d개를 구매했습니다.\n", lottos.size());
+        for (Lotto lotto : lottos) {
+            printLottoNumber(lotto);
+        }
+    }
+
+    private static void printLottoNumber(Lotto lotto) {
+        StringBuilder output = new StringBuilder("[");
+        List<Integer> numbers = lotto.getNumbers();
+        numbers.forEach(number -> {
+            output.append(number.toString()).append(", ");
+        });
+        output.setLength(output.length() - 2);
+        output.append("]");
+        System.out.println(output);
     }
 }
