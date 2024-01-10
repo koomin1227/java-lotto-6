09aff26e867251619b29ff71639e0dfa9ed16b9d
koomin1227 <koomin1227@naver.com>
2024-01-10 23:21:39 +0900

    feat: LottoMachine class 구현

---
+++ src/main/java/lotto/Domain/LottoMachine.java
@@ -0,0 +1,35 @@
+package lotto.Domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.MatchingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private final ArrayList<Lotto> lottos = new ArrayList<>();
+    private int price;
+    private int count;
+
+    public LottoMachine(int price) {
+        this.price = price;
+        this.count = price / 1000;
+    }
+
+    public ArrayList<Lotto> publishLotto(int count) {
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    public WinningResult calculateWinningResults(WinningNumbers winningNumbers) {
+        WinningResult result = new WinningResult(price);
+        for(Lotto lotto:lottos) {
+            MatchingResult matchResult = lotto.getMatchNumber(winningNumbers);
+            result.updateResult(matchResult);
+        }
+        return result;
+    }
+}
