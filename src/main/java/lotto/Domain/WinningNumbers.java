8adf9de66965c467c065f7895154e8b1c5d5470f
koomin1227 <koomin1227@naver.com>
2024-01-10 20:40:52 +0900

    refactor: Lotto 클래스를 상속 하도록 수정

--- src/main/java/lotto/Domain/WinningNumbers.java
+++ src/main/java/lotto/Domain/WinningNumbers.java
@@ -5,52 +5,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class WinningNumbers {
-    private final List<Integer> winningNumbers;
+public class WinningNumbers extends Lotto{
     private Number bonusNumber;
-    public WinningNumbers(ArrayList<String> winningNumbers) {
-        validate(winningNumbers);
-        this.winningNumbers = winningNumbers.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
-    private void validate(ArrayList<String> winningNumbers) {
-        if (winningNumbers.size() != 6) {
-            throw new IllegalArgumentException("6개가 아님");
-        }
-        for (String winningNumber : winningNumbers) {
-            if (!isNumeric(winningNumber)) {
-                throw new IllegalArgumentException("숫자로만 이루어져있지 않음");
-            }
-            if (!isValidLottoNumber(winningNumber)) {
-                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
-        }
-        if (isDouble(winningNumbers)) {
-            throw new IllegalArgumentException("숫자가 중복됨");
-        }
+    public WinningNumbers(ArrayList<Integer> winningNumbers) {
+        super(winningNumbers);
     }
 
-    private static boolean isNumeric(String input) {
-        return input.matches("\\d+");
-    }
-    private static boolean isValidLottoNumber(String input) {
-        int num = Integer.parseInt(input);
-        return num > 0 && num <= 45;
-    }
-
-    private boolean isDouble(List<String> input) {
-        HashSet<String> numbersSet = new HashSet<>(input);
-        return numbersSet.size() != input.size();
-    }
-
-    public List<Integer> getWinningNumbers() {
-        return winningNumbers;
-    }
-
-    public void setBonusNumber(String number) {
+    public void setBonusNumber(int number) {
         Number bonusNumber = new Number(number);
-        if (this.winningNumbers.contains(bonusNumber.getNumber())) {
+        if (numbers.contains(bonusNumber.getNumber())) {
             throw new IllegalArgumentException("숫자가 중복됨");
         }
         this.bonusNumber = bonusNumber;
